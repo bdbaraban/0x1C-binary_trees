d7ce959c65487f2c75eff5f692b6871b2916f995
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-03-11 18:13:07 -0700

    Tasks 11 & 12: Implemented size & leaves functions. Updated test files.

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,5 +7,13 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t size = 0;
 
+	if (tree)
+	{
+		size += 1;
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+	}
+	return (size);
 }
--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,5 +7,12 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-
+	size_t leaves = 0;
+	if (tree)
+	{
+		leaves += (!tree->left && !tree->right) ? 1 :0;
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
+	}
+	return (leaves);
 }
