f13df95b93ce0f74040db9afebc3e7cd47976123
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-03-11 19:10:01 -0700

    Tasks 13 & 14: Implemented nodes & balance factor functions. Updated test files.

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,11 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes -
+ * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree.
+ * @tree: A pointer to the root node of the tree to count the number of nodes.
  *
- * Return: 
+ * Return: If tree is NULL, the function must return 0, else return node count.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t nodes = 0;
 
+	if (tree)
+	{
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
 }
