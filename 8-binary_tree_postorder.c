9eb2a67aa84b650c24f1bec3704e297f7e596cf9
bdbaraban <375@holbertonschool.com>
2019-03-13 22:22:26 +0000

    Reduce line over 80 chars in documentation

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - Goes through a binary tree using post-order traversal.
+ * binary_tree_postorder - Traverses a binary tree using post-order traversal.
  * @tree: A pointer to the root node of the tree to traverse.
  * @func: A pointer to a function to call for each node.
  */
