232b358e751d26097c23574edbcbae56d4d2ed92
bdbaraban <375@holbertonschool.com>
2019-03-13 21:22:41 +0000

    Upload completed tasks 103-104 - rotation

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,11 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left -
+ * binary_tree_rotate_left - Left-rotates a binary tree.
+ * @tree: A pointer to the root node of the tree to rotate.
  *
- * Return: 
+ * Return: A pointer to the new root node after rotation.
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+	binary_tree_t *pivot, *tmp;
 
+	if (tree == NULL || tree->right == NULL)
+		return (NULL);
+
+	pivot = tree->right;
+	tmp = pivot->left;
+	pivot->left = tree;
+	tree->right = tmp;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+
+	return (pivot);
 }
--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,11 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_right -
+ * binary_tree_rotate_right - Right-rotates a binary tree.
+ * @tree: A pointer to the root node of the tree to rotate.
  *
- * Return: 
+ * Return: A pointer to the new root node after rotation.
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
+	binary_tree_t *pivot, *tmp;
 
+	if (tree == NULL || tree->left == NULL)
+		return (NULL);
+
+	pivot = tree->left;
+	tmp = pivot->right;
+	pivot->right = tree;
+	tree->left = tmp;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+
+	return (pivot);
 }
