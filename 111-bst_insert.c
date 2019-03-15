3078f57808ef14c527de5833e2cbb0299c3c14c7
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-03-14 20:12:48 -0700

    Task 25: Implemented BST insert. It works but feel free to prettify.

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,120 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * bst_insert -
+ * bst_insert - Inserts a value in a Binary Search Tree.
+ * @tree: A double pointer to the root node of the BST to insert the value.
+ * @value: The value to store in the node to be inserted.
  *
- * Return: 
+ * Return: a pointer to the created node, or NULL on failure.
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
 	if (tree)
 	{
 		bst_t *curr = *tree;
+
 		if (curr == NULL)
-		{
 			return (*tree = (bst_t *)binary_tree_node(curr, value));
-		}
-		else if (value <= curr->n)
+
+		if (value == curr->n)
+			return (NULL);
+
+		if (value < curr->n)
 		{
 			if (curr->left != NULL)
 				return (bst_insert(&curr->left, value));
 			return (curr->left = (bst_t *)binary_tree_node(curr, value));
 		}
+
 		/*else if (value > (*curr)->n) */
 		if (curr->right != NULL)
 			return (bst_insert(&curr->right, value));
 		return (curr->right = (bst_t *)binary_tree_node(curr, value));
 	}
-	else
-		return (NULL);
-}
-		/*if (*tree == NULL)
-		{
-			new = (bst_t *)binary_tree_node(*tree, value);
-			binary_tree_print(*tree);
-		}
-		else if (value < (*tree)->n)
-		{
-			new = bst_insert(&((*tree)->left), value);
-		}
-		else if (value > (*tree)->n)
-		{
-			new = bst_insert(&((*tree)->right), value);
-		}*/
-	/*	else
-		{
-			new = (bst_t *)binary_tree_node(*tree, value);
-			for (tmp = (*tree)->left->left; tmp; tmp = tmp->left)
-				;
-			tmp->left = new;
-			binary_tree_print(*tree);
-		}
-*/
-
-/**
- * binary_tree_insert_left - Inserts a node as a left-child of
- *                           of another in a binary tree.
- * @parent: A pointer to the node to insert the left-child in.
- * @value: The value to store in the new node.
- *
- * Return: If parent is NULL or an error occurs - NULL.
- *         Otherwise - a pointer to the new node.
- *
- * Description: If parent already has a left-child, the new node
- *              takes its place and the old left-child is set as
- *              the left-child of the new node.
- */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new;
-
-	if (parent == NULL)
-		return (NULL);
-
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
-		return (NULL);
-
-	if (parent->left != NULL)
-	{
-		new->left = parent->left;
-		parent->left->parent = new;
-	}
-	parent->left = new;
-
-	return (new);
-}
-
-/**
- * binary_tree_insert_right - Insert a node as the right-child
- *                            of another in a binary tree.
- * @parent: A pointer to the node to insert the right-child in.
- * @value: The value to store in the new node.
- *
- * Return: If parent is NULL or an error occurs - NULL.
- *         Otherwise - a pointer to the new node.
- *
- * Description: If parent already has a right-child, the new node
- *              takes its place and the old right-child is set as
- *              the right-child of the new node.
- */
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
-{
-	binary_tree_t *new;
-
-	if (parent == NULL)
-		return (NULL);
-
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
-		return (NULL);
-
-	if (parent->right != NULL)
-	{
-		new->right = parent->right;
-		parent->right->parent = new;
-	}
-	parent->right = new;
-
-	return (new);
+	return (NULL);
 }
