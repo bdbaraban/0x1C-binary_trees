24ab8669570f32de839dacb29bbba02f155b2af8
bdbaraban <375@holbertonschool.com>
2019-03-15 19:34:40 +0000

    Ignore duplicate values in received array
    Co-authored-by: Fernando Gonzalez <542@holbertonschool.com>

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -10,15 +10,23 @@
 bst_t *array_to_bst(int *array, size_t size)
 {
 	bst_t *tree = NULL;
-	size_t i;
+	size_t i, j;
 
 	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
-		if (bst_insert(&tree, array[i]) == NULL)
-			return (NULL);
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] == array[i])
+				break;
+		}
+		if (j == i)
+		{
+			if (bst_insert(&tree, array[i]) == NULL)
+				return (NULL);
+		}
 	}
 
 	return (tree);
